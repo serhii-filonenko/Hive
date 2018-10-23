a503c8548af3eeb9cc1f4a50df2b3658461cf334
Lench Volodymyr <volodymyr.lench@binary-studio.com>
2018-10-23 18:54:15 +0300

    add forward of columns

--- forward_engineering/helpers/generalHelper.js
+++ forward_engineering/helpers/generalHelper.js
@@ -1,122 +1,54 @@
 'use strict'
-const fs = require('fs');
-const path = require('path');
-const TAB_SIZE = 2;
 
-const tab = (text, count = 1) => {
-	const space = ' '.repeat(count * TAB_SIZE);
+const buildStatement = (mainStatement) => {
+	let composeStatements = (...statements) => {
+		return statements.reduce((result, statement) => result + statement, mainStatement);
+	};
 
-	return text.split('\n').map(string => space + string).join('\n');
+	const chain = (...args) => {
+		if (args.length) {
+			composeStatements = composeStatements.bind(null, getStatement(...args));
+
+			return chain;
+		}
+
+		return composeStatements();
+	};
+
+	const getStatement = (condition, statement) => {
+		if (condition) {
+			return '\n' + indentString(statement);
+		}
+
+		return '';
+	};
+
+	return chain;
 };
 
-const retrivePropertyFromConfig = (config, tab, propertyName, defaultValue = "") => ((config || [])[tab] || {})[propertyName] || defaultValue;
+const getName = (entity) => entity.code || entity.collectionName || entity.name || '';
+const getTab = (tabNum, configData) => Array.isArray(configData) ? (configData[tabNum] || {}) : {};
+const indentString = (str, tab = 4) => str.split('\n').map(s => ' '.repeat(tab) + s).join('\n');
 
-const retrieveContainerName = (containerConfig) => retrivePropertyFromConfig(
-		containerConfig, 0, "code", 
-		retrivePropertyFromConfig(containerConfig, 0, "name", "")	
-	);
-const retrieveEntityName = (entityConfig) => retrivePropertyFromConfig(
-	entityConfig, 0, "code",
-	retrivePropertyFromConfig(entityConfig, 0, "collectionName", "")
-);
-const retrieveUDF = (containerConfig) => retrivePropertyFromConfig(containerConfig, 1, "UDFs", []);
-const retrieveUDA = (containerConfig) => retrivePropertyFromConfig(containerConfig, 2, "UDAs", []);
-const retrieveIndexes = (entityConfig) => retrivePropertyFromConfig(entityConfig, 1, "SecIndxs", []);
-const getTableNameStatement = (keyspaceName, tableName) => getNameWithKeyspace(keyspaceName, `"${tableName}"`);
-const getNameWithKeyspace = (keyspaceName, name) => `${(keyspaceName) ? `"${keyspaceName}".` : ""}${name}`;
+const descriptors = {};
+const getTypeDescriptor = (typeName) => {
+	if (descriptors[typeName]) {
+		return descriptors[typeName];
+	}
 
-const getConfig = (pathToConfig) => {
 	try {
-		const config = fs.readFileSync(path.join(__dirname, "..", "..", pathToConfig));
-
-		return JSON.parse(config.toString().replace(/\/\*[\s\S]*?\*\//g, ""));
+		descriptors[typeName] = require(`../../types/${typeName}.json`);
+		
+		return descriptors[typeName];
 	} catch (e) {
 		return {};
 	}
 };
 
-const cacheResult = (method) => {
-	let result;
-
-	return (...args) => {
-		if (!result) {
-			result = method(...args);
-		}
-
-		return result;
-	}
-};
-
-const getFieldLevelConfig = cacheResult(() => getConfig(path.join("properties_pane", "field_level", "fieldLevelConfig.json")));
-
-const getTypesConfig = cacheResult(() => {
-	const getName = typeFile => typeFile.replace(/\.json/, "");
-	const typeDir = path.join(__dirname, "..", "..", "types");
-	const types = fs.readdirSync(typeDir);
-
-	return types.reduce((typesMap, fileName) => {
-		typesMap[getName(fileName)] = getConfig(path.join("types", fileName));
-
-		return typesMap;
-	}, {});
-});
-
-const getTypeConfig = (type) => getTypesConfig()[type];
-
-const getFieldConfig = (type, property) => {
-	const fieldLevelConfig = getFieldLevelConfig().structure;
-
-	if (!fieldLevelConfig[type]) {
-		return;
-	}
-	const propertyData = fieldLevelConfig[type].find(config => config.propertyKeyword === property);
-
-	return propertyData;
-};
-
-const eachField = (jsonSchema, callback) => {
-	const resultSchema = Object.assign({}, jsonSchema);
-
-	const eachProperty = (properties, callback) => {
-		return Object.keys(properties).reduce((resultSchema, propertyName) => {
-			const property = properties[propertyName];
-
-			resultSchema[propertyName] = callback(eachField(property, callback), propertyName);
-
-			return resultSchema;
-		}, {});
-	};
-	const eachItem = (items, callback) => {
-		return items.map((item, i) => {
-			return callback(eachField(item, callback), i);
-		});
-	};
-
-	if (resultSchema.hasOwnProperty('properties')) {
-		resultSchema.properties = eachProperty(resultSchema.properties, callback);
-	}
-
-	if (resultSchema.hasOwnProperty('items')) {
-		const items = Array.isArray(resultSchema.items) ? resultSchema.items : [resultSchema.items];
-
-		resultSchema.items = eachItem(items, callback);
-	}
-
-	return resultSchema;
-};
-
-
 module.exports = {
-	tab,
-	retrieveContainerName,
-	retrieveEntityName,
-	retrieveUDF,
-	retrieveUDA,
-	retrieveIndexes,
-	retrivePropertyFromConfig,
-	getTableNameStatement,
-	getFieldConfig,
-	getTypeConfig,
-	getNameWithKeyspace,
-	eachField
+	buildStatement,
+	getName,
+	getTab,
+	indentString,
+	getTypeDescriptor
 };
